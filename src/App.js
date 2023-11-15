c831ce3db564e3e18b531a37fc8e1cd08858edaf
JensHVF <jenshvfrid@gmail.com>
2023-11-16 00:30:40 +0100

    what am i doing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Confetti from "./Confetti";
 
-const shareMessage = "I just ran my first container using Docker";
+const shareMessage = "WOW";
 const shareLink = "https://docker.com/";
 
 const App = () => {
